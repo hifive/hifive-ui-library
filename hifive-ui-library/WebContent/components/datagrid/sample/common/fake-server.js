563a70f5d4ae051c2e1bd17903d0f46b80533426
Takashi Mitsubuchi <mitsubuchi.takashi.9ys@jp.nssol.nssmc.com>
2014-10-08 11:18:40 +0900

    datagrid のサンプルページのデザインを一新

---
+++ hifive-ui-library/WebContent/components/datagrid/sample/common/fake-server.js
@@ -0,0 +1,270 @@
+// ---- Fake Server ---- //
+(function() {
+	/**
+	 * トークン更新の有無を指定するリクエストヘッダーのキー
+	 */
+	var KEY_UPDATE_TOKEN = 'X-BERT-UPDATE-TOKEN';
+	h5.u.obj.expose('bert.fw', {
+		/**
+		 * リクエストヘッダにトークンを更新しないフラグをセットして通信を行う
+		 *
+		 * @param {String} url URL
+		 * @param {Object} option jQuery.ajaxに渡すオプションオブジェクト
+		 * @returs {JqXHRWrapper} jqXHRWrapperオブジェクト
+		 * @memberOf bert.fw
+		 */
+		ajax: function(url, option) {
+			var opt = option || {};
+			if (typeof url === 'object') {
+				// 第一引数にオプションオブジェクトが指定されていた場合
+				opt = url;
+			} else {
+				opt.url = url;
+			}
+			opt.beforeSend = function(xhr) {
+				// トークンの更新をしないようにリクエストヘッダにフラグを格納する
+				xhr.setRequestHeader(KEY_UPDATE_TOKEN, false);
+			};
+			return h5.ajax(opt);
+		}
+	});
+})(jQuery);
+
+(function($) {
+
+	//ランダム数生成
+	var random = function(range) {
+		return Math.floor(Math.random() * range);
+	};
+
+	//氏名マスタ
+	var nameMst = ['山田 太郎'
+					, '加藤 次郎'
+					, '山田 一郎'
+					, '石井 三郎'
+					, '三浦 四郎'
+					, '山田 花子'
+					, '長谷川 恵子'
+					, '山田 栄子'
+					, '山田 咲'
+					, '野崎 太郎'
+					, '森 次郎'
+					, '加藤 四郎'
+					, '野崎 三郎'
+					, '松本 四郎'
+					, '野崎 花子'
+					, '石井 恵子'
+					, '野崎 栄子'
+					, '野崎 咲'
+					, '森 太郎'
+					, '鈴木 次郎'
+					, '石井 一郎'
+					, '鈴木 三郎'
+					, '吉田 四郎'
+					, '加藤 花子'
+					, '鈴木 恵子'
+					, '鈴木 栄子'
+					, '長谷川 咲'
+					, '山本 太郎'
+					, '石井 次郎'
+					, '加藤 一郎'
+					, '山本 三郎'
+					, '前田 四郎'
+					, '山本 花子'
+					, '山本 恵子'
+					, '石井 栄子'
+					, '山本 咲'
+					, '佐藤 太郎'
+					, '加藤 次郎'
+					, '佐藤 一郎'
+					, '長谷川 三郎'
+					, '佐藤 四郎'
+					, '加藤 栄子'
+					, '森 恵子'
+					, '佐藤 栄子'
+					, '森 咲'
+					, '長谷川 太郎'
+					, '佐々木 次郎'
+					, '石井 次郎'
+					, '長谷川 三郎'
+					, '佐々木 四郎'
+					, '加藤 咲'
+					, '佐々木 恵子'
+					, '長谷川 栄子'
+					, '三浦 咲'
+					, '清水 太郎'
+					, '三浦 次郎'
+					, '松田 一郎'
+					, '岡本 三郎'
+					, '金子 四郎'
+					, '高木 花子'
+					, '近藤 恵子'
+					, '藤本 栄子'
+					, '村田 咲'
+					];
+	var nameMstLength = nameMst.length;
+
+	//名前取得
+	var getName = function(){
+		return nameMst[random(nameMstLength)];
+	}
+
+	//配属マスタ
+	var placeMst= ['東京', '大阪', '名古屋', '福岡'];
+	var placeMstLength = placeMst.length;
+
+	//配属取得
+	var getPlace = function(){
+		return placeMst[random(placeMstLength)];
+	}
+
+	//部署マスタ
+	var positionMst=['第一事業部', '第二事業部', '営業部', '総務部', '人事部', '広報部', '経理部', '企画部', '技術部', '開発部', '研究部'];
+	var positionMstLength = positionMst.length;
+
+	//部署取得
+	var getPosition = function(){
+		return positionMst[random(positionMstLength)];
+	}
+
+	var TEL_AREA_CODE=['090', '080'];
+	//電話番号取得
+	var getTelNo = function(){
+
+		var telNo = '';
+		telNo+= TEL_AREA_CODE[random(2)];
+
+		telNo+='-';
+		for(var i = 0; i < 4; i++){
+			telNo += '0123456789'.charAt(Math.random() * 10);
+		}
+		telNo+='-';
+		for(var i = 0; i < 4; i++){
+			telNo += '0123456789'.charAt(Math.random() * 10);
+		}
+		return telNo;
+	}
+
+	//メールアドレス
+	var getMailAddress = function(){
+		var address = '';
+		for(var i = 0; i < 4; i++){
+			address += 'abcdefghijklmnopqrstuvwxyz'.charAt(Math.random() * 25);
+		}
+		address+='.';
+		for(var i = 0; i < 4; i++){
+			address += 'abcdefghijklmnopqrstuvwxyz'.charAt(Math.random() * 25);
+		}
+		address += random(10);
+		address += '@nssol.co.jp';
+		return address;
+	}
+
+	var data = [];
+	for (var i = 1; i <= 10000; i++) {
+		var now = new Date();
+		var time = now.getTime() + random(100) * 1000 * 60 * 60 * 24;
+		var date = new Date(time);
+
+		var year = String(date.getFullYear());
+		var month = String(date.getMonth() + 1);
+		var day = String(date.getDate());
+
+		month = '0' + month;
+		month = month.substring(month.length - 2);
+
+		day = '0' + day;
+		day = day.substring(day.length - 2);
+
+		var ymd = year + '/' + month + '/' + day;
+
+		var record = {
+			id: 'J' + ("00000"+i).slice(-5).toString(),
+			name: getName(),
+			place : getPlace(),
+			position : getPosition(),
+			tel: getTelNo(),
+			mail : getMailAddress()
+		};
+
+		data.push(record);
+	}
+
+	var sortKey = null;
+	var sortOrder = 'asc';
+
+	var sorted = $.extend([], data);
+
+	var sort = function(option) {
+		var requestSortKey = option.sortKey;
+		var requestSortOrder = option.sortOrder;
+		if (typeof requestSortKey === 'undefined') {
+			requestSortKey = null;
+		}
+		if (requestSortOrder == null) {
+			requestSortOrder = 'asc';
+		}
+
+		if (sortKey === requestSortKey && sortOrder === requestSortOrder) {
+			return;
+		}
+
+		sortKey = requestSortKey;
+		sortOrder = requestSortOrder;
+
+		if (sortKey == null) {
+			sorted = $.extend([], data);
+			return;
+		}
+
+		sorted.sort(function(x, y) {
+			var xValue = x[sortKey];
+			var yValue = y[sortKey];
+
+			var base = (sortOrder === 'asc') ? 1 : -1;
+
+			if (xValue < yValue) {
+				return -1 * base;
+			}
+			if (xValue === yValue) {
+				return 0;
+			}
+			return 1 * base;
+		});
+	};
+
+
+	var createResult = function(option) {
+
+		sort(option);
+
+		var result = {
+			list: sorted.slice(option.start, option.end)
+		};
+
+		if (option.requireTotalCount) {
+			result.totalCount = sorted.length;
+		}
+
+		return result;
+	};
+
+
+	var originAjax = bert.fw.ajax;
+
+	bert.fw.ajax = function(url, option) {
+		if (url !== '/api/sample3') {
+			return originAjax(url, option);
+		}
+
+		var deferred = h5.async.deferred();
+
+		setTimeout(function() {
+			var result = createResult(option.data);
+			deferred.resolve(result);
+		}, 200);
+
+		return deferred.promise();
+	};
+
+})(jQuery);
---
+++ hifive-ui-library/WebContent/components/datagrid/sample/sample3/scroll-grid-controller.js
@@ -0,0 +1,201 @@
+/*global h5, bert*/
+
+// ---- Logic ---- //
+(function($) {
+	var DATA_URL = '/api/sample3';
+
+	var gridSampleLogic = {
+		__name: 'bert.fw.grid.sample.GridLogic',
+
+		loadData: function() {
+			var def = this.deferred();
+			bert.fw.ajax(DATA_URL, {
+				dataType: 'json',
+				data : '',
+			}).done(function(data){
+				def.resolve(data.list);
+			});
+			return def.promise();
+		}
+	};
+
+	h5.core.expose(gridSampleLogic);
+
+})(jQuery);
+
+// ---- Controller ---- //
+(function($) {
+
+	var scrollGridController = {
+
+		// --- コントローラの設定 --- //
+
+		__name: 'scrollGridController',
+
+		__meta: {
+			_gridController: {
+				rootElement: '#grid'
+			}
+		},
+
+		// --- プロパティ --- //
+
+		_gridController: h5.ui.components.datagrid.ScrollGridController,
+
+		_gridLogic: bert.fw.grid.sample.GridLogic,
+
+		// --- プライベートなメソッド --- //
+
+		_updateSelectDataIds: function() {
+			var selectedDataIds = this._gridController.getSelectedDataIds();
+			if(selectedDataIds.length < 15){
+				this.$find('#selectedDataIds').text(selectedDataIds);
+			}else{
+				this.$find('#selectedDataIds').text(selectedDataIds.slice(0, 14) + '....');
+			}
+		},
+
+		// --- ライフサイクル関連メソッド --- //
+
+		__ready: function() {
+
+			//gridの初期化
+			this._gridLogic.loadData().done(this.own(function(data) {
+				this._gridController.init({
+					data: data,
+					idKey: 'id',
+					rowHeight: 25,
+					gridHeight: 651,
+					gridWidth : 800,
+					headerColumns: 2,
+					verticalScrollStrategy: 'index',
+					columns: [
+					{
+						propertyName: '_selectCheckBox',
+						header: '<input id="selectAll" type="checkbox">',
+						width: 40,
+						formatter: function(cellData) {
+							if (cellData.isHeaderRow) {
+								return cellData.value;
+							}
+							var dataId = cellData.dataId;
+							var checked = cellData.selected ? 'checked' : '';
+							var html = '<input type="checkbox"';
+							html += ' data-bert-data-id="' + dataId + '"';
+							html += ' ' + checked;
+							html += '>';
+							return html;
+						},
+						sortable: false
+					}, {
+						propertyName: 'id',
+						header: '社員ID',
+						width: 100,
+						sortable: true
+					},
+					{
+						propertyName: 'name',
+						header: '氏名',
+						width: 150,
+						sortable: true
+					},
+					{
+						propertyName: 'place',
+						header: '配属',
+						width: 100,
+						sortable: true
+					},
+					{
+						propertyName: 'position',
+						header: '部署',
+						width: 100,
+						sortable: true
+					},
+					{
+						propertyName: 'tel',
+						header: '電話番号',
+						width: 120,
+						sortable: true
+					},
+					{
+						propertyName: 'mail',
+						header: 'メールアドレス',
+						width: 200,
+						sortable: true
+					}
+					]
+				});
+			}));
+
+		},
+
+
+		'#grid renderGrid' :function(){
+
+			//bootstrapのtableストライプ
+			this.$find('.grid-header-columns table').addClass('table table-striped');
+			this.$find('.grid-main-box table').addClass('table table-striped');
+
+			var selectedDataIds = this._gridController.getSelectedDataIds();
+			if(selectedDataIds.length > 0){
+				//選択データがある場合はチェックON状態にする
+				//データ変更後の再描画でヘッダも再描画されチェックボックスがOFF状態に戻るため
+				this.$find('#selectAll')[0].checked = true;
+			}
+		},
+
+		// --- イベントハンドラメソッド --- //
+
+		// 選択
+		'input[type="checkbox"]:not(#selectAll) change': function(context, $el) {
+			var isSelected = $el.prop('checked');
+
+			// data による取得は数値変換できる場合はしてしまうので文字列に直す
+			var dataId = String($el.data('bertDataId'));
+
+			if (isSelected) {
+				this._gridController.selectData(dataId);
+
+				//1つでも選択がある場合はヘッダチェックボックスをONにする
+				this.$find('#selectAll')[0].checked = true;
+			} else {
+				this._gridController.unselectData(dataId);
+			}
+
+			this._updateSelectDataIds();
+		},
+
+		// 全選択 or 全選択解除
+		'#selectAll click': function(context, $el) {
+			$el[0].checked ?  this._gridController.selectAllData() : this._gridController.unselectAllData();
+			this._updateSelectDataIds();
+		},
+
+		// rowId から元データの取得
+		'td[data-grid-is-header-row="false"] click': function(context, $el) {
+			var rowId = $el.data('gridRowId');
+			var data = this._gridController.getCachedData(rowId);
+
+			alert(JSON.stringify(data));
+		},
+
+		//選択社員IDをアラート表示
+		'#showSelectedDataIds click' : function(context, $el){
+			var selectedDataIds = this._gridController.getSelectedDataIds();
+			alert(selectedDataIds.length +'件選択しています\n'+selectedDataIds);
+		},
+
+		// ヘッダをクリックしたらソート
+		'td.grid-header[data-h5-dyn-grid-is-sortable-column="true"] click': function(context, $el) {
+			var key = $el.data('data-h5-dyn-grid-property-name');
+			this._gridController.sort(key);
+		}
+
+	};
+
+	// ---- Init ---- //
+	$(function() {
+		h5.core.controller('body', scrollGridController);
+	});
+
+})(jQuery);
