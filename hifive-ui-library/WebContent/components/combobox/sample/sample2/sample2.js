3c66901e8840aa37db183a5fc2624823964c4ac0
fukudayasuo <fukuda@pc0594.o.sysrdc.ns-sol.co.jp>
2015-12-04 18:59:34 +0900

    combobox最新化。サンプル追加。

---
+++ hifive-ui-library/WebContent/components/combobox/sample/sample2/sample2.js
@@ -0,0 +1,32 @@
+(function($) {
+	var DATA_URL = './combobox.json';
+	var comboboxSampleController = {
+
+		__name: 'sample2.SampleController',
+
+		/** コンボボックスコントローラ */
+		_accountComboBoxController: h5.ui.components.combobox.ComboBoxController,
+
+		__meta: {
+			_accountComboBoxController: {
+				rootElement: 'input[name="accountcode"]'
+			}
+		},
+
+		__ready: function() {
+
+			this._accountComboBoxController.init({
+				url: DATA_URL
+			});
+		}
+	};
+
+	h5.core.expose(comboboxSampleController);
+
+})(jQuery);
+
+
+// ---- Init ---- //
+$(function() {
+	h5.core.controller('body', sample2.SampleController);
+});
